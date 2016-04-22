037c6e4a94b810d811558d3e21f30145d34b5897
justForL <layer_james@163.com>
2016-04-22 21:50:52 +0800

    Initial Commit

---
+++ PianKeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PianKeDemo
+//
+//  Created by James on 16/4/22.
+//  Copyright © 2016年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
